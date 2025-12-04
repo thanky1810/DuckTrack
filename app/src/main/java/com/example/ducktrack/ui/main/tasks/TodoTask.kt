2fb249057f8347b3d8ad5ebc07d61640ce7761c9
AkinaAoi <dungvutri25@gmail.com>
2025-12-04 23:09:35 +0700

    Update save tasks in Firebase

--- app/src/main/java/com/example/ducktrack/ui/main/tasks/TodoTask.kt
+++ app/src/main/java/com/example/ducktrack/ui/main/tasks/TodoTask.kt
@@ -1,16 +1,24 @@
-// FILE: TodoTask.kt
 package com.example.ducktrack.ui.main.tasks
 
 import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.PropertyName // <-- NHỚ IMPORT CÁI NÀY
 
-// Đổi tên từ Task -> TodoTask để tránh trùng với Firebase Task
 data class TodoTask(
     @DocumentId
-    @get:Exclude
     val id: String = "",
+
     val description: String = "",
-    val isCompleted: Boolean = false,
-    val isPinned: Boolean = false,
+
+    // Ánh xạ trường 'completed' trên Firebase vào biến 'isCompleted'
+    @get:PropertyName("completed")
+    @set:PropertyName("completed")
+    var isCompleted: Boolean = false,
+
+    // Ánh xạ trường 'pinned' trên Firebase vào biến 'isPinned'
+    @get:PropertyName("pinned")
+    @set:PropertyName("pinned")
+    var isPinned: Boolean = false,
+
     val date: Long = 0L
 )
